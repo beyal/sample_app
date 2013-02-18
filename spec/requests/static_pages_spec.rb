fb4d51a4c8d4d15085db50f2b101bcbc679e807d
beyal <bar_eyal@hotmail.com>
2013-02-18 21:47:39 +0200

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,6 +32,17 @@ describe "Static pages" do
         end
       end
 
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+
       describe "user should not be able to see delete links of other users microposts" do
         let(:user2) { FactoryGirl.create(:user) }
         let!(:m1) { FactoryGirl.create(:micropost, user: user2, content: "Foo") }
